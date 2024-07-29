b4c928a0987fa96effc75af9f8638184caabcef9
kimsunjae <woal9844@gmail.com>
2024-07-29 20:31:38 +0900

    design : 메인 페이지 퍼블리싱

---
+++ src/components/common/NavBar.tsx
@@ -0,0 +1,29 @@
+import homeIcon from '@assets/homeIcon.png'
+import personIcon from '@assets/personIcon.png'
+import resumeIcon from '@assets/resumeIcon.png'
+import { useNavigate } from 'react-router-dom'
+
+function NavBar() {
+  const navigate = useNavigate()
+
+  return (
+    <footer className="shadow-md flex">
+      <button className="flex-1 flex flex-col justify-center items-center active:bg-gray-200 p-4">
+        <img src={resumeIcon} className="p-1" />
+        <p className="text-[#B7B7B7] font-bold text-xs">이력서 공유</p>
+      </button>
+      <button
+        className="flex-1 flex flex-col justify-center items-center active:bg-gray-200 p-4"
+        onClick={() => navigate('/')}>
+        <img src={homeIcon} className="p-1" />
+        <p className="text-[#FE654F] font-bold text-xs">홈</p>
+      </button>
+      <button className="flex-1 flex flex-col justify-center items-center active:bg-gray-200 p-4">
+        <img src={personIcon} className="p-1" />
+        <p className="text-[#B7B7B7] font-bold text-xs">마이페이지</p>
+      </button>
+    </footer>
+  )
+}
+
+export default NavBar
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,31 @@
+import bgStartJob from '@assets/bgStartJob.png'
+import bgStartResume from '@assets/bgStartResume.png'
+import NavBar from '@components/common/NavBar'
+
 function MainPage() {
-  return <main>hello</main>
+  return (
+    <>
+      <main className="p-6 shadow-md">
+        <div className="h-[70%] w-[90%] relative hover:scale-105 transition-transform duration-300">
+          <img src={bgStartJob} className="w-full h-full absolute" />
+          <p className="absolute text-[#FEAA61] text-lg font-bold top-8 left-5">
+            빛나래와의 대화를 통해
+          </p>
+          <p className="absolute text-2xl font-bold top-16 left-5">나만의 새로운 진로를</p>
+          <p className="absolute text-2xl font-bold top-24 left-5">찾아 볼까요?</p>
+          <button className="absolute p-2 px-6 font-bold text-white text-2xl flex justify-center items-center bottom-5 left-5 bg-[#FEAA61] rounded-3xl">
+            시작하기
+          </button>
+        </div>
+        <div className="h-[30%] mt-4 w-[90%] relative hover:scale-105 transition-transform duration-300">
+          <img src={bgStartResume} className="w-full h-full" />
+          <p className="absolute text-white text-2xl font-bold top-8 left-5">Ai로 자기소개서를</p>
+          <p className="absolute text-white text-2xl font-bold top-16 left-5">생성해 보세요!</p>
+        </div>
+      </main>
+      <NavBar />
+    </>
+  )
 }
 
 export default MainPage
