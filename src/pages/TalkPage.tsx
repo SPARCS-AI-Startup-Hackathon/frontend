58513cf27a3cff2d2e2f09b6ec4cdc2b27c97d95
kimsunjae <woal9844@gmail.com>
2024-07-30 23:10:14 +0900

    feat : navigate 설정

--- src/pages/TalkPage.tsx
+++ src/pages/TalkPage.tsx
@@ -1,12 +1,25 @@
 import bgTalk from '@assets/images/bgTalk.png'
+import bitnarae_default from '@assets/images/bitnarae_default.png'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+import { useNavigate } from 'react-router-dom'
+// import bitnarae_talk from '@assets/images/bitnarae_talk.png'
 
 function TalkPage() {
+  const navigate = useNavigate()
+
   return (
     <div
       className="w-dvw max-w-[375px] h-dvh m-auto bg-cover bg-center flex flex-col items-center justify-between shadow-md"
       style={{ backgroundImage: `url(${bgTalk})` }}>
-      <div></div>
-      <button className="w-[90%] text-white text-xl bg-[#FA8D43] p-2.5 px-10 rounded-3xl mb-10 active:bg-orange-500">
+      <div className="w-full flex justify-start p-4 py-5">
+        <FaArrowLeftLong
+          size="28"
+          className="cursor-pointer text-customOrange"
+          onClick={() => navigate('/main')}
+        />
+      </div>
+      <img src={bitnarae_default} />
+      <button className="w-[90%] text-white text-xl font-bold bg-[#FA8D43] p-2.5 px-10 rounded-3xl mb-10 active:bg-orange-500">
         시작하기
       </button>
     </div>
