5b671479b2a0305788df74a4f7d1c055bbed9918
kimsunjae <woal9844@gmail.com>
2024-07-31 13:33:01 +0900

    feat : 오디오 재생 상태일때 이미지 동적으로 변환

--- src/pages/TalkStartPage.tsx
+++ src/pages/TalkStartPage.tsx
@@ -1,12 +1,37 @@
 import bgTalk from '@assets/images/bgTalk.png'
 import bitnarae_default from '@assets/images/bitnarae_default.png'
+import bitnarae_talk from '@assets/images/bitnarea_talk.png'
 import DynamicTalkBox from '@components/talk/DynamicTalkBox'
+import { useEffect, useState } from 'react'
 import { FaArrowLeftLong } from 'react-icons/fa6'
 import { useNavigate } from 'react-router-dom'
+import useAudioStore from 'store/store'
 import '../styles/talk.css'
 
 function TalkStartPage() {
   const navigate = useNavigate()
+  const { isPlaying } = useAudioStore()
+  const [currentImage, setCurrentImage] = useState<string>(bitnarae_default)
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setCurrentImage((prevImage) =>
+          prevImage === bitnarae_default ? bitnarae_talk : bitnarae_default,
+        )
+      }, 500)
+    } else {
+      setCurrentImage(bitnarae_default)
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  }, [isPlaying])
 
   return (
     <div
@@ -21,7 +46,7 @@ function TalkStartPage() {
       </div>
       <div className="relative flex flex-col items-center">
         <DynamicTalkBox />
-        <img src={bitnarae_default} className="mt-8" />
+        <img src={currentImage} className="mt-8" />
       </div>
       <button className="w-[90%] text-white text-xl font-bold bg-[#FA8D43] p-2.5 px-10 rounded-3xl mb-10 active:bg-orange-500">
         시작하기
