e60894b9d4d5aeb81495962bd5bab3b365850868
kimsunjae <woal9844@gmail.com>
2024-08-01 01:30:15 +0900

    feat : 방문여부에 따른 온보딩 페이지 리다이렉트

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,40 @@ import TalkPage from '@pages/TalkPage'
 import TalkStartPage from '@pages/TalkStartPage'
 import TestPage from '@pages/TestPage'
 import WelcomePage from '@pages/WelcomePage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <CheckVisited />
         <Routes>
-          <Route path="/" element={<OnBoardPage />}></Route>
-          <Route path="/welcome" element={<WelcomePage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
-          <Route path="/main" element={<MainPage />}></Route>
-          <Route path="/talk" element={<TalkPage />}></Route>
-          <Route path="/talk_start" element={<TalkStartPage />}></Route>
-          <Route path="/test" element={<TestPage />}></Route>
+          <Route path="/" element={<OnBoardPage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/talk" element={<TalkPage />} />
+          <Route path="/talk_start" element={<TalkStartPage />} />
+          <Route path="/test" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
     </div>
   )
+}
+
+function CheckVisited() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const isVisited = localStorage.getItem('isVisited') === 'true'
+    if (isVisited) {
+      navigate('/welcome')
+    }
+  }, [navigate])
+
+  return null
 }
 
 export default App
